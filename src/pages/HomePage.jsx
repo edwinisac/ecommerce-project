b60bf843965cf243a528af98557978a9229c4b90
edwinisac <edwinisackanjirathinkal@gmail.com>
2025-10-22 21:18:39 +0530

    get cart from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,33 @@
+import axios from 'axios';
+import { useEffect ,useState} from 'react';
 import { Header } from "../components/Header";
-import {products} from '../../starting-code/data/products'
+// import {products} from '../../starting-code/data/products'
 import "./HomePage.css";
 
 export function HomePage() {
+  const [products,setProducts]=useState([]);
+  const [cart,setCart]=useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/products')
+    .then((response)=>{
+        setProducts(response.data);
+        
+    })
+    axios.get('http://localhost:3000/api/cart-items')
+    .then((response)=>{
+      setCart(response.data)
+    })
+  },[])
+
+
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="favicons/home.png" />
 
       <title>Ecommerce Project</title>
-      <Header />
+      <Header cart={cart}/>
 
       <div className="home-page">
         <div className="products-grid">
